2cf30596e8602293cba502ac56816d8a85b12b14
sanjeev kumar <ksanjeev34@gmail.com>
2016-04-02 14:18:06 +0530

    Update Clustering.R

--- Clustering.R
+++ Clustering.R
@@ -1,3 +1,6 @@
+# Author : Sanjeev Kumar
+# Modification History : 1.04.2016 Creation Date
+
 # Netflix Movie Dataset
 # There are two types of data 
 #                 1) Every Movie in Netflix db has ranking from all users
@@ -16,9 +19,8 @@
 # distance. If the variables are not on the same scale then we need the normalization.
 
 
-
+# Clustering is mostly used in absence of target varaibles to search for relationship between input variables
 # Hierarchical Clustering(Using Euclidean and Centroid): This technioque combines the two nearest clusters in the same cluster.
-# 
 # Load the Movie lens dataset and prepare the dataset
 movies = read.table("Movielens.txt", header=FALSE, sep="|", quote="\"")
 str(movies)
@@ -71,4 +73,114 @@ cluster2$Title[1:10]
 #       5. Re-compute cluster centroids
 #       6. Repeat 4 and 5 until no improvement is made.
 
+# Cluster MRI Images by tissue substances and locate pathological anatomlies.
+# Image Segmentation : Divide the image into regions/clusters corresponding to individual surfaces, objects or natural parts of object.
+# Segments should be meaningful(most important). Various methods are proposed to segment the images. 
+# 1. Clustering Methods : Partition image to clusters based on differences in pixel color , intensity or texture.
+# 2. Edge Detection : Based on the  detection of discontinuity, such as an abrupt change in grey level in grey-scale images.
+# 3. Region-growing methods : Divide image into regions, then sequentially merge sufficiently similar regions.
 
+# Let start with a simple example of clustering Pixels using greyscale images.
+# Greyscale image : Matrix of pixel intensity values ranging from 0(black) to 1(white). For 8bit/pixel we have 256 color levels.
+# Greyscale image segmentation is done by clustering pixels according to their intensity values.
+# What are the inputs to clustering algo? Observation are vector containing each pixel intensity value and a single variable i.e intensity value.
+# Most Important step before feeding intensity values to the clustering algo is morphing our data i.e lump all intensity values into a single vector.
+
+flower_data = read.csv("flower.csv", header = FALSE)
+str(flower_data)
+
+# So what is observed in str(flower_data). r treats the rows as observations and columns as variables.
+# So need to change the data type to matrix
+flowerMatrix = as.matrix(flower_data)
+
+# as.vector stores the matrix as vector column wise if with default options. 
+# Q: Why not we directly convert the data to vector instead of converting data to matrix and then vector? Hint: Ans lies in above comments.
+flowerVector = as.vector(flowerMatrix)
+distance = dist(flowerVector, method = "euclidean")
+str(flowerVector)
+# ward.D method is the minimum variance method which trying to find compact and spherical clusters
+clusterIntensity = hclust(distance, method = "ward.D")
+plot(clusterIntensity)
+image(flowerClusters, axes=FALSE)
+rect.hclust(clusterIntensity, k=3, border="red")
+flowerClusters = cutree(clusterIntensity, k =3)
+flowerClusters
+tapply(flowerVector, flowerClusters, mean)
+
+# This will convert flower cluster into a 50 x 50 matrix 
+dim(flowerClusters) = c(50,50)
+image(flowerClusters, axes=FALSE)
+image(flowerMatrix, axes=FALSE, col = grey(seq(0,1,length=256)))
+
+###############################################################################################################################
+# Now segment the MRI image of a healthy patient using hierarchical clustering
+healthy = read.csv("healthy.csv", header = FALSE)
+healthyMatrix = as.matrix(healthy)
+str(healthyMatrix)
+image(healthyMatrix, axes=FALSE, col = grey(seq(0,1,length=256)))
+
+# convert healthy matrix to vector
+healthyVector = as.vector(healthyMatrix)
+
+# The first step in hierarchical clustering is computing the distance matrix
+distance = dist(healthyVector, method = "euclidean")
+str(healthyVector)
+n = length(healthyVector)
+# So a distance vector have n*(n-1)/2 values
+n*(n-1)/2
+# if each cell in the matrix takes 4 bytes then the number showing represents the amount of memory 
+# required in GB's
+# 2 : Symmetric matrix , +n for diagonal elements.
+(2*n*(n-1)*2+n)/(1024*1024*1024)
+# So it means that by this way we cannot use hierarchial clustering. So is there any pther means to use clustering.
+# k-means clustering 
+# 1. Specify the number of clusters. But how? Setting the number of clusters depends on what are you trying to extract from 
+#    the image. e.g no of grey maater or white matter
+k = 5
+set.seed(1)
+KMC = kmeans(healthyVector, centers = k, iter.max = 1000)
+str(KMC)
+healthyClusters = KMC$cluster
+# How can we obtain the mean intensity value within each of the 5 clusters.
+KMC$centers
+KMC$size
+
+# If we notice that laargest cluster is cluster number 3 of size 133162 but have the lowest mean intensity value.
+# This means that the possible cluster number 3 can be the darkest shade in the MRI image.And again notice on the 
+# mean intensity of each cluster , all are less than 0.5 i.e the image is pretty dark.
+# Now convert healthyCluster Vector to matrix
+dim(healthyClusters) = c(nrow(healthyMatrix), ncol(healthyMatrix))
+image(healthyClusters, axes=FALSE, col = rainbow(k))
+
+# ANother way of selecting the number of clusters is using the scree plot which works for any clustering algorithm.
+# A standard scree plot have number of cluster on the x-axis and within-cluster  sum of squares on y axis.
+# What is this within-cluster sum of squares ? For a given cluster it is the squared distance between each point and the 
+# the centroid of the cluster.
+
+KMC2 = kmeans(healthyVector, centers = 2, iter.max = 1000)
+KMC2$withinss
+NumClusters = seq(2,10,1)
+SumWithinss = sapply(2:10, function(x) sum(kmeans(healthyVector, centers=x, iter.max=1000)$withinss))
+# You will find in the scree plot that after 5 clusters the bend is smoother. So number of cluster =5 may be adequate.
+plot(NumClusters, SumWithinss, type="b")
+
+
+################ Now DETECT TUMOR WITH OLIGODENDROGLIOMA : A tumor that commonly occurs in the front lobe of the brain.
+# The brain biopsy is the only definite diagnosis of this tumor. So MRI gudiance is key in determining it's location and geometry.
+tumor = read.csv("tumor.csv", header = FALSE)
+tumorMatrix = as.matrix(tumor)
+tumorVector = as.vector(tumorMatrix)
+
+# So how we proceed? 
+# Apply the clustering results of healthy patient MRI image to this one or treat healthy vector as a training set and
+# tumorVector as the test set. To do that we require a new package called flexclust
+install.packages("flexclust")
+library("flexclust")
+
+# The flexclust package contains the library called kcca which stand for k centroid cluster analysis.
+# We need to convert the information from clustering algorithm to the object of the class kcca and need before doing predict 
+# function on the tumor test set data.
+KMC.kcca = as.kcca(KMC, healthyVector)
+tumorClusters = predict(KMC.kcca, newdata = tumorVector)
+dim(tumorClusters) = c(nrow(tumorMatrix), ncol(tumorMatrix))
+image(tumorClusters, axes=FALSE, col=rainbow(k))
